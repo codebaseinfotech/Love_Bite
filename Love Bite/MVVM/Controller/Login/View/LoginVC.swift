2d802abf02a3133fd89e00fd31d52768e476c08e
Jenil iOS <jenil.codebase@gmail.com>
2025-08-25 13:45:23 +0530

    new changes

---
+++ Love Bite/MVVM/Controller/Login/View/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Love Bite
+//
+//  Created by iMac on 25/08/25.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
