072143c5b9ff78e99a83ecab1c122c8e46ac194e
Jenil iOS <jenil.codebase@gmail.com>
2025-08-28 16:10:54 +0530

    Show us your best shots Screen done

--- Love Bite/MVVM/Controller/Show us your best shots/Cell/collectionViewPhotosCell.swift
+++ Love Bite/MVVM/Controller/Show us your best shots/Cell/collectionViewPhotosCell.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+protocol CollectionViewPhotosCellDelegate: AnyObject {
+    func didTapAddPhoto()
+    func didTapRemovePhoto(at index: Int)
+}
+
 class collectionViewPhotosCell: UICollectionViewCell {
 
     @IBOutlet weak var imgProfile: UIImageView!
@@ -14,9 +19,31 @@ class collectionViewPhotosCell: UICollectionViewCell {
     @IBOutlet weak var viewPhoto: UIView!
     @IBOutlet weak var viewAddPhoto: UIView!
     
+    @IBOutlet weak var btnRemove: UIButton!
+    
+    weak var delegate: CollectionViewPhotosCellDelegate?
+    var index: Int?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(addPhotoTapped))
+        viewAddPhoto.isUserInteractionEnabled = true
+        viewAddPhoto.addGestureRecognizer(tapGesture)
+        
+        // Remove button action
+        btnRemove.addTarget(self, action: #selector(removePhotoTapped), for: .touchUpInside)
         // Initialization code
+    }
+    
+    @objc private func addPhotoTapped() {
+        delegate?.didTapAddPhoto()
+    }
+    
+    @objc private func removePhotoTapped() {
+        if let idx = index {
+            delegate?.didTapRemovePhoto(at: idx)
+        }
     }
 
 }
--- Love Bite/MVVM/Controller/Show us your best shots/View/ShowusyourbestshotsVC.swift
+++ Love Bite/MVVM/Controller/Show us your best shots/View/ShowusyourbestshotsVC.swift
@@ -11,6 +11,10 @@ class ShowusyourbestshotsVC: UIViewController {
 
     @IBOutlet weak var collectionViewPhotos: UICollectionView!
     
+    let imagePicker = UIImagePickerController()
+    
+    var selectedPhotos: [UIImage] = []
+    
     let SlidarsectionInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
     let SlidaritemsPerRow : CGFloat = 1
     var SlidarflowLayout: UICollectionViewFlowLayout {
@@ -25,8 +29,8 @@ class ShowusyourbestshotsVC: UIViewController {
             
             _SlidarflowLayout.sectionInset = self.SlidarsectionInsets
             _SlidarflowLayout.scrollDirection = UICollectionView.ScrollDirection.vertical
-            _SlidarflowLayout.minimumInteritemSpacing = 2
-            _SlidarflowLayout.minimumLineSpacing = 2
+            _SlidarflowLayout.minimumInteritemSpacing = 5
+            _SlidarflowLayout.minimumLineSpacing = 10
         }
         
         // edit properties here
@@ -35,6 +39,8 @@ class ShowusyourbestshotsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
 
         collectionViewPhotos.register(UINib(nibName: "collectionViewPhotosCell", bundle: nil), forCellWithReuseIdentifier: "collectionViewPhotosCell")
         collectionViewPhotos.delegate = self
@@ -53,10 +59,11 @@ class ShowusyourbestshotsVC: UIViewController {
     
 }
 
-extension ShowusyourbestshotsVC: UICollectionViewDelegate, UICollectionViewDataSource {
+extension ShowusyourbestshotsVC: UICollectionViewDelegate, UICollectionViewDataSource, CollectionViewPhotosCellDelegate {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        // All photos + 1 "Add" cell
+        return selectedPhotos.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -64,15 +71,72 @@ extension ShowusyourbestshotsVC: UICollectionViewDelegate, UICollectionViewDataS
         
         cell.viewPhoto.isHidden = true
         cell.viewAddPhoto.isHidden = true
+        cell.delegate = self
+        cell.index = indexPath.item
         
-        if indexPath.item == 0 {
+        if indexPath.item < selectedPhotos.count {
+            // Photo cell
             cell.viewPhoto.isHidden = false
-        } else if indexPath.item == 1 {
+            cell.imgProfile.image = selectedPhotos[indexPath.item]
+            cell.btnRemove.isHidden = false
+        } else {
+            // Add Photo cell
             cell.viewAddPhoto.isHidden = false
+            cell.btnRemove.isHidden = true
         }
         
         return cell
     }
     
+    // Add photo tapped
+    func didTapAddPhoto() {
+        let alert = UIAlertController(title: "Choose Option", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallery()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     
+    // Remove photo tapped
+    func didTapRemovePhoto(at index: Int) {
+        if index < selectedPhotos.count {
+            selectedPhotos.remove(at: index)
+            collectionViewPhotos.reloadData()
+        }
+    }
+}
+
+// MARK: - Image Picker
+extension ShowusyourbestshotsVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func openGallery() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagePicker.sourceType = .photoLibrary
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            // 🔹 Add image to array
+            selectedPhotos.append(selectedImage)
+            collectionViewPhotos.reloadData()
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
